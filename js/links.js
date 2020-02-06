62a807efc37450830d43e7cbeb80d687b1a99b84
Paolo Santos <paolo.niccolo.santos@gmail.com>
2020-02-06 20:43:49 +0800

    Initial Commit

---
+++ js/links.js
@@ -0,0 +1,337 @@
+var links = [
+"be1","be2","be3","be4","be5","be6","be7","be8","be9",
+"cs1","cs10","cs11","cs2","cs3","cs4","cs5","cs6","cs7","cs8","cs9",
+"k1","k2","k3","k4","k5","k6","cs7","cs8","cs9",
+"li1","li2","li3","li4","li5","li6","li7","li8","li9","li10",
+"mg1","mg10","mg11","mg12","mg2","mg3","mg4","mg5","mg6","mg7","mg8","mg9",
+"na1","na2","na3","na4","na5","na6","na7","na8","na9",
+"sr3","sr5","sr7","sr8","sr9","sr10","sr11","sr12","sr13","sr14","sr15",
+"rb1","rb2","rb3","rb4","rb5","rb6","rb7","rb8","rb9","rb10",
+]
+
+var title = [
+"Sugoi Sugoi",
+"PRIDE: The Filipino Youth's Advocacy for LGBT Rights",
+"Sogie and why you need it too",
+"Earthquake",
+"Choose Local",
+"Sogie Quality Now",
+"IC Change",
+"West Philippine Sea",
+"The Climate Connection", // end of Be
+
+"The Dangers of Socal Media \"Games\"",
+"Medi-kool",
+"Animals have feeling too: Animals Too",
+"Public Knowledge is Public Power",
+"VaxxPack",
+"Mental Illness/Disorders (and How to Identify Them)",
+"Non-Crammers",
+"Everything You Need to Know About Sogie",
+"Advoca-Sea",
+"Eyesberg",
+"Sincerely", // end of Cs
+
+"ICT in Education",
+"Silenced: Know where the line is",
+"Safetree",
+"getHelp.com",
+"The Philippines Drainage System: It's an absolute mess",
+"Time Management",
+"Metro Manila Traffic: Why is it so bad?",
+"The SOGIE Bill",
+"Pros and Cons of ICT in School", // end of K
+
+"Flamazonia and How to Stop It",
+"Political Compass",
+"Colonial Mentality",
+"Are We Ready for Sogie",
+"Parada: Gabay sa Maluwag na Kalsada",
+"Games of Sea",
+"Bridging the Gap",
+"Centro: Spread Love and Care",
+"Refugee Crisis",	
+"Sharing is for Everyone", // end of Li
+
+"Mental Health and ICT",
+"Modern Relationships",
+"Verde Enviro",
+"Biolumen: Shining the Spotlight on the Problems around Biodiversity",
+"Anonymous: Look for the Light",
+"Black Bauhinia",
+"Bullying",
+"Aquacibus",
+"Poverty: Why is it a problem and how to fix it",
+"Stop Climate Change",
+"Bye Homes, Biomes",
+"Waste Management", // End of Mg
+
+"Classrooms and Social Media",
+"Floodwatch",
+"Stress",
+"Scale of Trash",
+"HTML: How to Mend a Life",
+"Internet Safety",
+"Be a SavePH Advocate",
+"The Mind in ICT: How Mental Health is Affected by ICT",
+"Understanding Anxiety", // End of Na
+
+/**************************************
+SR
+**************************************/
+"Mobile Legends", //sr3
+"Take A Break", //sr5
+"The Human Digestive System", //sr7
+"Sightlines", //sr8
+"KALACHUCHI LANE", //sr9
+"BiologyBot", //sr10
+"Chain Reaction: Game", //sr11
+"Boys' Residence Hall Main", //sr12
+"The Digestive System", //sr13
+"Pisay Eats", //sr14
+"Graphika 2019", //sr15
+
+/**************************************
+RB
+**************************************/
+"Metaguide", //rb1
+"Grade 9 Academic Directory", //rb2
+"Favorite Places in Pisay", //rb3
+"Rubidium 2023", //rb4
+"Rubidium", //rb5
+"Types of Class Dynamics", //rb6
+"Landmarks of PSHS-MC", //rb7
+"The Pisay Spice", //rb8
+"Fake Messenger", //rb9
+"Corona Virus", //rb10
+]
+
+var desc = [
+"The website talks about deforestation and another website called Team Trees, and aims to sell stickers to donate to said Team Trees.",
+"A website to inculcate the Filipino personages for enlightenment concerning the LGBT community and their virtues who passionately love their country and its people who are under colonization.",
+"This infographic website focuses on the Sexual Orientation and Gender Identity Expression Equality Bill or SOGIE Bill and how we should spread awareness to the issue of oppression of the LGBT community.",
+"This website aims to inform more people about the serious dangers of earthquakes, how to deal with them, and what might be coming for us in the future.",
+"The purpose of our website [ChooseLocalPH] is to raise awareness regarding our local farmers' status after the implementation of Rice Tarrification Law.",
+"Our website is dedicated to raising awareness on the issues faced by the LGBT+ community, including one of the most recent ones, the passing of the SOGIE bill.",
+"With the news becoming more accessible to the mass media, our website aims to spread awareness regarding a very real global issue that is Climate Change. ",
+"Our goal of this website is to spread awareness on the issues on the West Philippine Sea.",
+"This site acts as a summary of the ongoing climate crisis, and it aims to spread awareness and promote the preservation of our planet.", // end of Be
+
+"Our site aims to inform and warn others about dangerous internet games, detailing examples such as the Blue Whale Challenge and Pass Out Challenge— as well as how to avoid them.",
+"The Philippine Healthcare System in Context",
+"Animal cruelty could be happening all around you, but since they don't have voices of their own, you might not realize it.",
+"A website that helps the user in identifying forms of fake news and infornms the user of viable ways to fight back against fake news.",
+"VAXXPACK is the no.1 company delivering vaccines throughout the Philippines and providing accurate, up-to-date information regarding immunization.",
+"This website evaluates and analyzes mental illnesses and shows how they could be coped with.",
+"Our website is about cramming- more specifically, information and statistics on cramming, and how one can stop with the help of ICT.",
+"The proper education of SOGIE, or Sexual Orientation and Gender Identity Expression, has been one of the most pressing issues in the Philippines, which has resulted to the miseducation of Filipino citizens and the misinformed people bashing others based on their SOGIE.",
+"Listen to their pleas, save the Philippine seas, so join our advocasea !!",
+"Eyesberg is a website that lets people be aware about all of the effects of global warming including the melting of glaciers that are currently melting, sea levels rising up abruptly, how much forest fires are going on right now, what are the animals that are about to go extinct and many more.",
+"A compilation of short literary pieces written by a grade 9 student.", // end of Cs
+
+"This is an informative and interactive website about the application and importance of ICT in Education",
+"It's an informative website about our group's advocacy: anti-rape and domestic abuse",
+"Our mission for this website is to open the minds of people neglecting their obligations to protect Mother Earth, the people benefitting from the environment, and the flora and fauna living around us.",
+"GetHelp aims to use ICT to make mental health help more accessible to everyone.",
+"This website discusses about the situation of the Philippine's poor drainage systems located throughout the country; their advantages, disadvantages, water security, and others.",
+"Our website aims to help fellow Pisay students in managing their time. <3",
+"It is an informative website regarding the widespread issue plaguing our country: traffic.",
+"Our website aims to spread knowledge and awareness about the SOGIE Bill.",
+"A website about ICT.", // End of K
+
+"Our website aims to raise awareness and open people's eyes to the crisis happening to the Amazon and other parts of the world that is global warming and its effects.",
+"Enlightening the public about politics",
+"Various examples of Colonial Mentality in the Philippines and how to solve it",
+"Get to know more in detail the SOGIE Bill, the LGBT+ Community, and what they have in store for our future. ",
+"Ang Parada, gabay sa kalsada - ito ay isang app na tumutugon sa pangangalangan ng bansang pilipinas - ang kakulangan ng parking space!",
+"This website aims to show the controversies and circumstances that occurred in the preparation and in the midst of the 2019 South East Asian or SEA games ",
+"Bridging the Gap - Why the rich and poor don't know each other, and how we can fix that.",
+"In relation to world hunger, the website aims to send donations (with the help of people's charity) to Filipino families suffering from hunger.",
+"Info about the present refugee crises and how to help them.", 
+"Come help us as we do our best to save, preserve, and protect our environment! Donate now!", // End of Li
+
+"This website aims to inform the audience about Mental Health - how it benefits from advancements in ICT and an overview of Mental Health in the context of the PSHS Community.",
+"A website that compiles our opinions and findings on ICT's effects on relationships and how you should use it.",
+"Verde Enviro is a database where you can find news, media, information, and even products beneficial to the environment, or increases awareness of the environment.",
+"Biolumen is a website that aims to showcase the different causes to the loss of biodiversity in our planet, as well as educate them on possible solutions to said problems.",
+"It's a website made to support people who have depression and anxiety issues.",
+"This is a website promoting awareness and a quick summary of the current situation of the Hong Kong protests, along with providing links to sites that give constant updates about it.",
+"This website aims to promote awareness on the causes and effects of bullying for everyone.",
+"Aquacibus aims to help bring awareness to the millions around the world who do not have access to clean, safe water and food. ",
+"This website aims to make people aware of the growing issue of poverty.",
+"In our online Eco warrior community website, you can learn, communicate, suggest innovations, and even report unusual environmental happenings, no limit, anywhere in the globe.",
+"Bye Home, Biomes gives prominence to the destruction of our planet and the saving the we can do. ",
+"Our website is about waste management, and it's aim is to inform people of the various ways of managing waste.",// end of Mg
+
+"This website includes the research about the effects of different social media during classes and the results of surveys regarding the use of social media for classes and its effects.",
+"Our website seeks to spread awareness on the many characteristics of the most common natural disaster in the Philippines.",
+"Stress; a common feeling of being overwhelmed, worried, or run-downed.",
+"This website can help you visualize the scale of trash. ",
+"A website that aims to promote suicide prevention.",
+"The internet is not always safe, here are some tips on how to keep away from danger.",
+"As a website, SavePH aims to spread awareness regarding major global calamities, such as climate change and the plastic epidemic.",
+"This website is created for and by students to inform people about Mental Health and why it matters. ",
+"It talks about anxiety and its symptoms, manifestations, and possible treatments", // End of Na
+
+/**************************************
+SR
+**************************************/
+"Description of how ML helps in the stressful Pisay life.", //sr3
+"Literally just a blog about things, nothing else really...", //sr5
+"Overview of human digestive system", //sr7
+"A site describing the English theatre club of PSHS-MC (Sightlines) as of AY 2019-2020.", //sr8
+"This website shows the basic facts of the Kalachuchi flower and how it is special to PISAY students.", //sr9
+"This site will give you knowledge about biology! It will give you knowledge, but also some facts about me. This will make you more interested in this science. Please view this for our grades hehehe <3", //sr10
+"Chain Reaction: The Game", //sr11
+"This site answers your questions about PSHS-MC BRHM. It will tell you about the life of dormers residing in BRHM.", //sr12
+"My website aims to inform people about the digestive system, one of the many organ systems of the human body.", //sr13
+"Concept for a Pisay-based food delivery app (still under construction)", //sr14
+"Our own Website about Graphika 2019", //sr15
+
+/**************************************
+RB
+**************************************/
+"This website discusses some tips and tricks on building a website based on the lectures and topics up to the Second Quarter of CS3.", //rb1
+"A site meant to document and outline the subjects Biology, Chemistry, Physics. Contains topics of each subject.", //rb2
+"This website aims to explore Pisay from the viewpoint of students who, despite being stressed and tired in its environment, still see the beauty in its campus. As of the moment, many constructions are ongoing, and there are lots of beloved places that have been abolished; but even so, we would still like to include some of those places to reminisce and look back fondly at good times. All that being said, we hope you enjoy our little website.", //rb3
+"Our site is about our section, Rubidium. It is about what the rubidium element is, our class schedule, and who comprises the entirety of Rb '23. It also has a page reserved for the authors.", //rb4
+"The website features the class of Rubidium reimagined into the roster of Super Smash Bros game.", //rb5
+"Our website, Types of Class Dynamics, shows the many different types of class activities that can make a student feel welcome in the classroom", //rb6
+"It is rushed. It is incomplete and it won't look like a normal website. This was also rushed...", //rb7
+"Food blog type site that features combinations of food from Pisay's three main concessionaires following a theme.", //rb8
+"It is an imitation of the website messenger.com. Also 'chatting' is actually a thing in this website.", //rb9
+"A website dedicated to raise awareness and communicate the latest updates about the corona virus and it's impact with the school community.", //rb10
+]
+
+var urls = [
+"https://fortheearth.000webhostapp.com",
+"https://pride-lgbt-rights.000webhostapp.com/",
+"http://sogie-and-why-you-need-it-too.000webhostapp.com/",
+"https://phearthquakes.000webhostapp.com/",
+"http://chooselocalph.000webhostapp.com/",
+"https://sogieequalitynow.000webhostapp.com/htdocs/index.html",
+"http://icchange.000webhostapp.com/",
+"http://csproject.aghamroad.com",
+"https://theclimateconnection.000webhostapp.com",	// end of Be
+
+"https://dangers-of-social-media-games.000webhostapp.com/index.html",
+"https://medi-kool.000webhostapp.com/",
+"https://animalshavefeelingstoo.000webhostapp.com",
+"https://pubkispubp.000webhostapp.com/proj.html",
+"https://vaxxpack.000webhostapp.com/",
+"https://minecraftparodies.000webhostapp.com/Cs%202nd%20Qtr%20Website%20Project/index.html",
+"http://cramcramcram.000webhostapp.com/",
+"https://everything-you-need-toknow-about-sogie.000webhostapp.com/",
+"https://advocasea.000webhostapp.com",
+"https://eyesberg.000webhostapp.com/",
+"http://cs3q2proj.000webhostapp.com/",		// end of Cs
+
+"http://k15k24k282ndquarter.dx.am/project/",
+"http://silenced-findyourvoice.000webhostapp.com/",
+"http://safetree.orgfree.com",
+"https://zer0nu.github.io/q2-cs-project/",
+"https://marethyu115.github.io",
+"https://github.com/eiorii/k07k22k272ndQuarterProject",
+"https://floofboi.github.io",
+"https://nagimaeda.github.io/csproject2q/",
+"http://potassium131718.000webhostapp.com/",	// end of K
+
+"https://flamazoniaandhowtostopit.000webhostapp.com/",
+"https://politicalcompass.000webhostapp.com/",
+"http://colonialmentality.000webhostapp.com",
+"http://prideshikken.000webhostapp.com",
+"https://parada2020.000webhostapp.com/",
+"https://gamesofsea.ml",
+"https://egoisticaltruism.000webhostapp.com",
+"http://livewithcentro.000webhostapp.com",
+"http://refugee-crisis.ml/",
+"https://sharingisforeveryone.000webhostapp.com/",	// end of Li
+
+"http://bit.ly/2QCSProjZaraAng",
+"https://modern-relationships.000webhostapp.com/",
+"http://verdeenviro.000webhostapp.com/",
+"http://bit.ly/BIOLUMEN",
+"https://anonymous3395.github.io/CS3Project2-Mg01-Mg03-Mg15-/",
+"https://black-bauhinia-mg2023-1.000webhostapp.com",
+"https://q2prj-bullying-bermudez-consebido.000webhostapp.com/htdocs/homepage.html",
+"http://aquacibus.000webhostapp.com/",
+"https://poverty-awareness.000webhostapp.com/",
+"https://beanecowarrior.000webhostapp.com/",
+"http://bit.ly/byehomebiome",
+"https://csproject-slayer.000webhostapp.com/", // end of Mg
+
+"http://socialmediainclass.000webhostapp.com/",
+"http://flood.rf.gd/",
+"https://chaosbolt9999.github.io/meowt",
+"https://2ndquartercs3project.000webhostapp.com/",
+"https://howtomendalife.000webhostapp.com/Home.html",
+"https://cs3project.000webhostapp.com/",
+"https://save-ph.000webhostapp.com/",
+"https://mentalhealthandict.000webhostapp.com/",
+"https://understandinganxiety.000webhostapp.com/",  // end of Na
+
+/**************************************
+SR
+**************************************/
+"https://exonerated-conferen.000webhostapp.com/MLinPisay/index.html", //sr3
+"https://craggier-millions.000webhostapp.com/", //sr5
+"http://biology.22web.org/2q%20project/homepage/", //sr7
+"https://thesightlinesclub.000webhostapp.com/", //sr8
+"https://cmajorminor7th.github.io/CSKalachuchi/", //sr9
+"http://biologybot.freetzi.com/", //sr10
+"https://doom423.github.io/Chain-RX/", //sr11
+"https://ruru48.github.io/KnowBRHM/", //sr12
+"https://bio-csproj2ndquarter.000webhostapp.com/proj%20final%20final/proj.html", //sr13
+"https://pisayeats.000webhostapp.com/splashpage.html", //sr14
+"https://unhingedgoose.github.io/", //sr15
+
+/**************************************
+RB
+**************************************/
+"https://cs3q3projectmetaguide.000webhostapp.com/Home.html", //rb1
+"https://gr9biochemphysics.000webhostapp.com/index.html", //rb2
+"https://vivien1084.github.io/favplaceinpisay/", //rb3
+"http://rb2023.000webhostapp.com/", //rb4
+"http://tinniest-jewels.000webhostapp.com/", //rb5
+"http://types-of-class-dynamics.000webhostapp.com/", //rb6
+"https://doyouhearthepeoplesing.000webhostapp.com/Intro.html", //rb7
+"https://pisayspice.000webhostapp.com/", //rb8
+"https://rb18-messengerproject.000webhostapp.com/Messenger.html", //rb9
+"https://cantorial-holder.000webhostapp.com/index%20copy.html", //rb10
+]
+
+function dispProj(){
+	var x;
+	var genNum=[];
+	var numProj = Math.floor(Math.random() * links.length);
+	for (i=0;i<links.length;i++) {	
+		// update list of used number
+		genNum.push(numProj);
+		// check if the generated random number is already used.	
+		numProj = Math.floor(Math.random() * links.length)
+		x = genNum.indexOf(numProj)
+		if (genNum.length == links.length)
+			break;
+		while (x >= 0) {
+			numProj = Math.floor(Math.random() * links.length)
+			x = genNum.indexOf(numProj)
+		}		
+	}
+	
+	// construct content of the body
+	var tobeDisp = ""
+	for (i=0;i<links.length;i++) {
+	    tobeDisp +=	 
+			'<div class="gallery">'+
+			'<a target="_blank" href="' + urls[genNum[i]] +'">'+
+			'<img src="img\\'+links[genNum[i]] +'.png" /></a>'+
+		   	'<div class="desc"><span>'+title[genNum[i]]+'</span><br/><br/>'+desc[genNum[i]]+'</div>'+
+			'</div>'
+	}
+		
+	document.getElementById("pHolder").innerHTML = tobeDisp;
+}
+
+ 
+ 
+ 
